e2eb9eae1320fe497a98c93ec8b26e25af8149f6
LordOfPolls <ddavidallen13@gmail.com>
2019-02-04 20:37:03 +0000

    Added PDF support

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # DNDCorrupted-Text
 
-A simple python file that corrupts text to challenge your players. it can handle docx and txt files.
+A simple python file that corrupts text to challenge your players. It can handle ``.docx``, ``.txt`` and ``.pdf`` files.
 
-This script requires the modules ``docx2txt`` and ``tkinter`` to run
+This script requires the modules ``docx2txt``,``tkinter`` and ``PyPDF2`` to run
 
+##### Note on PDF support:
+This script will only ever read **2** pages at most. This is because processing PDF pages takes a long time, and it isnt effective to read an entire book just to corrupt it.
 ## Example output
-![Example Image](https://i.imgur.com/XnvV9gx.png)
+![Example Image](https://i.imgur.com/8hzTnvk.png)
--- corrupt.py
+++ corrupt.py
@@ -7,8 +7,9 @@ import tkinter as tk
 import sys
 import os
 import hashlib
-import pip
+import PyPDF2
 from tkinter import filedialog
+
 
 ### UTILITY FUNCTIONS ###
 def clear():
@@ -16,27 +17,50 @@ def clear():
     if not 'idlelib.run' in sys.modules:
         os.system("cls")
 
+
 def wrap(text):
     """Surrounds the text with = to make titles"""
     text = text.center(shutil.get_terminal_size().columns, "=")
     return text
+
 
 def read(path):
     """Reads the selected file into a string"""
     if ".docx" in path:
         print("Reading docx file")
         text = docx2txt.process(path)
-    else:
-        print("Reading generic file")
+    elif ".txt" in path:
+        print("Reading txt file")
         with open(path) as f:
             text = f.readlines()
+        pprint.pprint(text)
+    elif ".pdf" in path:
+        print("Reading PDF...")
+        file = open(path, 'rb')
+        PDF = PyPDF2.PdfFileReader(file)
+
+        # This script will only ever read 2 pages because reading pdf pages takes AGES
+        if PDF.numPages > 2:
+            print("Note: This code will only read two pages due to the fact PDFs take a long time to process")
+            input("Press return to continue")
+        text = PDF.getPage(0).extractText()
+        if PDF.numPages > 1:
+            text += "\n\n"
+            text += PDF.getPage(1).extractText()
+    else:
+        print("Unsupported filetype. This code only supports:\n\n.txt\n.pdf\n.docx")
+        main()
+        return
+        
     text = "".join(text)
     return text
+
 
 def generateSeed(text):
     """Generates a seed using the MD5 of the text, not necessary"""
     seed = str(hashlib.md5(text.encode()).hexdigest())
     random.seed(seed)
+
 
 def corrupt(text):
     """The code that actually corrupts the text"""
@@ -53,9 +77,9 @@ def corrupt(text):
     return "".join(text)
 
 
-### PREPERATION ### 
+### PREPARATION ###
 
-print(wrap("PREPERATION"))
+print(wrap("PREPARATION"))
 # load tkinter without creating a window in order to allow file dialogs
 root = tk.Tk()
 root.withdraw()
@@ -64,7 +88,10 @@ root.withdraw()
 def main():
     """The main function, calls all other functions"""
     print("Select a File to corrupt")
-    file_path = filedialog.askopenfilename(filetypes = (("Word Documents","*.docx *.txt"),("All Files","*.*")))
+    file_path = filedialog.askopenfilename(title="Select file to corrupt",
+                                           filetypes=(("Supported","*.docx *.txt *.pdf"),("All Files","*.*")))
+    if not file_path:
+        exit()
     text = read(file_path)
     generateSeed(text)
     print("{} Loaded...".format(os.path.basename(file_path)))
@@ -74,12 +101,9 @@ def main():
 
     ### OUTPUT ###
     print(wrap("CORRUPTED {}".format(os.path.basename(file_path).upper())))
-    print
     print("\n", corrupted)
     print(wrap(""))
 
+
 main()
-input("Press any key to exit")
-    
-    
-    
+input("Press return to exit")
